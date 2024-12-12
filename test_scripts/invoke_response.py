b80b83b014df1f618900b97d881fa981ceba30b9
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-12-12 03:38:26 -0300

    Update docs

--- test_scripts/invoke_response.py
+++ test_scripts/invoke_response.py
@@ -1,15 +1,15 @@
 import requests
 
 def make_get_request():
-    url = 'http://localhost:5678/webhook-test/logs/error'
+    url = 'http://localhost:5678/webhook-test/response'
     
     params = {
-        "prompt": "Isso não é uma piada",
+        "prompt": "What is this?",
         "chat_id": "chat123",
         "client_name": "test_client",
         "sfw": True,
         "sender_name": "Felipe Catapano",
-        "media": ""
+        "media": "https://cdn.awsli.com.br/600x1000/519/519712/produto/38217083/3cfa6a6c1e.jpg"
     }
 
     response = requests.get(url, params=params)
