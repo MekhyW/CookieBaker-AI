a1d28026b2481d66f0e06efef510100ecebe8c5d
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-12-12 02:52:55 -0300

    Fix logs/error bug

--- test_scripts/invoke_response.py
+++ test_scripts/invoke_response.py
@@ -1,25 +1,21 @@
 import requests
 
 def make_get_request():
-    url = 'http://localhost:5678/metrics'
+    url = 'http://localhost:5678/webhook-test/logs/error'
     
     params = {
-        'sfw': True,
-        'chat_title': 'Test Chat',
-        'chat_description': 'This is a test chat description',
-        'prompt': 'Test prompt for chatbot',
-        'sender_name': 'Test User',
-        'sender_isadmin': False,
-        'media': ''
+        "prompt": "Isso não é uma piada",
+        "chat_id": "chat123",
+        "client_name": "test_client",
+        "sfw": True,
+        "sender_name": "Felipe Catapano",
+        "media": ""
     }
 
     response = requests.get(url, params=params)
 
-    if response.status_code == 200:
-        print(f"Response Status: {response.status_code}")
-        print(f"Response Body: {response.text}")
-    else:
-        print(f"Failed to reach the webhook. Status Code: {response.status_code}")
+    print(f"Response Status: {response.status_code}")
+    print(f"Response Body: {response.text}")
 
 if __name__ == "__main__":
     make_get_request()
